26143eb41990b362e106492a7ca501c938957941
José Pedro W <josepw@hotmail.com>
2022-03-02 16:47:50 -0300

    Aula 1 da semana 7 tipos de variáveis JS

--- Week_6/JS para Web/main.js
+++ Week_6/JS para Web/main.js
@@ -1,34 +1,26 @@
+//exemplo de função toca som específica
 function tocaSomPom() {
 
     document.querySelector('#som_tecla_pom').play();
 
 }
 
+//toca som Genérico
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); //todas as teclas salvas como lista, constante
 
-listaDeTeclas[0].onclick = tocaSomPom;
-
-listaDeSons = ['#som_tecla_pom', '#som_tecla_clap', '#som_tecla_tim', '#som_tecla_puff', '#som_tecla_splash',
-    '#som_tecla_toim', '#som_tecla_psh', '#som_tecla_tic', '#som_tecla_tom'
-]
-
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]; //tecla clicada
-    const instrumento = tecla.classList[1]; //classe da tecla clicada
+    const instrumento = tecla.classList[1]; //classe da tecla clicada que é o nome do instrumento
     //template string
     const idAudio = `#som_${instrumento}`; //string iniciada com crase (concatena);
 
     tecla.onclick = function() { //função anônima para chamar o tocaSom apenas quando há interação
-        tocaSom(idAudio);
+        tocaSom(idAudio); //chama a função toca som
     };
-    contador++;
 
-    console.log(contador);
 }
---
+++ Week_7/Curso 1/Aula1/type-number.js
@@ -0,0 +1,20 @@
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
