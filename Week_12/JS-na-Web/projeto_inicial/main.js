d00b9f310de3e4e22e59de2f9418c52d4989826a
JosePw <josepw@hotmail.com>
2022-04-09 11:46:33 -0300

    Aula 1 ok JS na web

--- Week_12/JS-na-Web/projeto_inicial/main.js
+++ Week_12/JS-na-Web/projeto_inicial/main.js
@@ -1,8 +1,7 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
 
-const criarTarefa = (evento) => {
-
+const handLeNovoItem = (evento) => {
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
@@ -10,23 +9,38 @@ const criarTarefa = (evento) => {
     const valor = input.value
 
     const calendario = document.querySelector('[data-form-date]')
-    const data = calendario.value
-    console.log(data)
+    const data = moment(calendario.value) //formatando data usando lib moment
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = {
+        valor,
+        dataFormatada
+    }
+
+    const criaTarefa = criarTarefa(dados)
+
+    lista.appendChild(criaTarefa)
+    input.value = " "
+
+
+}
+
+const criarTarefa = ({ valor, dataFormatada }) => {
 
 
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
+
+    return tarefa
 
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handLeNovoItem)
